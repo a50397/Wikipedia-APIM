136d6dc0ddd00c78753a45c399d866eaf7c058a8
fokinko <foka@posteo.de>
2016-02-16 16:57:13 +0100

    debugging apis

--- js/connect.js
+++ js/connect.js
@@ -1,43 +1,85 @@
 $(document).ready(function(){
-    var IP = "http://10.100.103.31:8280/wikipedia/1.0.1";
+    var IP = "http://10.100.103.31:8280/wikipedia/1.0.0";
+    var autorization = "37c154c543d673d05c31a0af94c2f3a9";
 
-    function console(text, trieda, okno){
-        var p = $("<p></p>");
-        p.text(text);
-        p.addClass(trieda);
-        $(okno).append(p);
+    function consoleT(text, trieda, okno){
+        //var p = $("<p></p>");
+        //p.text(text);
+        //p.addClass(trieda);
+        $("#consoleInputText").text(text);
     }
 
     function consoleI(text, okno){
-        var p = $("<iframe>"+text+"</iframe>");
-        //p.html(text);
-        //RegExp("<body[^>]*>(.*?)<\/body>")
-        $(okno).append(text.match(/<body[^>]*>(.*?)<\/body>/g));
+        //var p = $("<iframe id='htmlReturn' />");
+        var reg = new RegExp(/<body.*?>([^]*?)<\/body>/g),
+            regH = new RegExp(/<head.*?>([^]*?)<\/head>/g);
+        var vonBody = reg.exec(text);
+        var vonHead = regH.exec(text);
+        //$(okno).append(p);
+        //$(p).wrap('<div class="embed-responsive embed-responsive-16by9"/>');
+        //$(p).addClass('embed-responsive-item');
+        //$(p).html = "";
+        var bodyFrame = $("#htmlReturn").contents().find("body");
+        var headFrame = $("#htmlReturn").contents().find("head");
+        if (vonHead[1])
+            $(headFrame).html(vonHead[1]);
+        if (vonBody[1])
+            $(bodyFrame).html(vonBody[1]);
     }
+
+    function wikiList(){
+        $("#serverIP").text(IP);
+
+        consoleT('Connecting to ' + IP, "input", "#console");
+        $.ajax({
+            url: IP,
+            headers: { "Authorization": "Bearer " + autorization},
+            //dataType: "xml",
+            method: "GET",
+            error: function(jqXHR, textStatus, errorThrown){
+                //consoleT(textStatus + ", " + jqXHR.status + ", ", "output", "#console");
+            }
+        }).complete(function(data) {
+            if (data.status == 200){
+                //consoleT(data.responseText,"ouput", "#console");
+                consoleI(data.responseText, "#console");
+            }
+            else {
+                consoleT("status: " + data.statusText + " " + data.status + " " + data.getAllResponseHeaders(), "output", "#console");
+            }
+        });
+    }; //end wiki
 
     function wiki(){
         $("#serverIP").text(IP);
-
-        console('Connecting to ' + IP, "input", "#console");
+        var searchText = $("#wikisearch").val();
+        var URL = "http://10.100.103.31:8280/wikipedia/1.0.1/?&search=" + searchText;
+        consoleT('Connecting to ' + URL, "input", "#console");
         $.ajax({
-            url: IP,
-            headers: { "Authorization": "Bearer e1d6169484d1bad2396c72e04d6f9f0f"},
-            dataType: "xml",
+            url: URL,
+            headers: { "Authorization": "Bearer " + autorization},
+            dataType: "json",
             method: "GET",
             error: function(jqXHR, textStatus, errorThrown){
-                //console(textStatus + ", " + jqXHR.status + ", ", "output", "#console");
+                //consoleT(textStatus + ", " + jqXHR.status + ", ", "output", "#console");
             }
         }).complete(function(data) {
-            if (data.statusText != "error"){
-                if (data.id){
-                    console("id: " + data.id + " content: " + data.content, "output", "#console");
-                } else {
-                    console(data.responseText,"ouput", "#console");
-                }
+            if (data.status == 200){
+                //consoleT(data.responseText,"ouput", "#console");
+                consoleI(data.responseText, "#console");
             }
             else {
-                console("status: " + data.statusText + " " + data.status + " " + data.getAllResponseHeaders(), "output", "#console");
+                consoleT("status: " + data.statusText + " " + data.status + " " + data.getAllResponseHeaders(), "output", "#console");
             }
         });
-    } //end wiki
+    }; //end wiki
+
+    $("#searchButton").click(function(event){
+        if ($("#wikisearch").val())
+            wiki();
+        else
+            wikiList();
+    });
+
+    wikiList();
 })
