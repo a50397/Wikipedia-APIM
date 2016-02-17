2cb892454b7f2bc2034e31f9237d9732f63bc6f1
fokinko <foka@posteo.de>
2016-02-17 22:58:05 +0100

    calculator added

--- js/connect.js
+++ js/connect.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
     var IP = "http://10.100.103.31:8280/wikipedia/1.0.0";
-    var autorization = "37c154c543d673d05c31a0af94c2f3a9";
+    var autorization = "66d68c2f15fe899eb15d28e482c7f1a1";
 
     function consoleT(text, trieda, okno){
         //var p = $("<p></p>");
@@ -21,9 +21,9 @@ $(document).ready(function(){
         //$(p).html = "";
         var bodyFrame = $("#htmlReturn").contents().find("body");
         var headFrame = $("#htmlReturn").contents().find("head");
-        if (vonHead[1])
+        if (vonHead)
             $(headFrame).html(vonHead[1]);
-        if (vonBody[1])
+        if (vonBody)
             $(bodyFrame).html(vonBody[1]);
     }
 
@@ -53,20 +53,36 @@ $(document).ready(function(){
     function wiki(){
         $("#serverIP").text(IP);
         var searchText = $("#wikisearch").val();
-        var URL = "http://10.100.103.31:8280/wikipedia/1.0.1/?&search=" + searchText;
+        var URL = "http://10.100.103.31:8280/wikipedia/search/1.0.0/" + searchText;
         consoleT('Connecting to ' + URL, "input", "#console");
         $.ajax({
             url: URL,
             headers: { "Authorization": "Bearer " + autorization},
-            dataType: "json",
+            //dataType: "json",
             method: "GET",
             error: function(jqXHR, textStatus, errorThrown){
-                //consoleT(textStatus + ", " + jqXHR.status + ", ", "output", "#console");
+                consoleT(textStatus + ", " + jqXHR.status + ", ", "output", "#console");
             }
         }).complete(function(data) {
             if (data.status == 200){
                 //consoleT(data.responseText,"ouput", "#console");
-                consoleI(data.responseText, "#console");
+                if (!data.responseJSON) {
+                    consoleI(data.responseText, "#console");
+                } else {
+                    var vysledok = data.responseJSON;
+                    var text = "<body>";
+                    text = text + "<h1> Hľadaný výraz: " + vysledok[0] + "</h1>";
+                    text = text + "<h2> Počet výsledkov: " + vysledok[1].length + "</h2>";
+                    for (var i = 0; i < vysledok[1].length; i++){
+                        text += "<div><h3>" + vysledok[1][i] + "</h3>" +
+                        "<p>" + vysledok[2][i] +"</p>"  +
+                        "<a href='" + vysledok[3][i] + "'>" + vysledok[3][i] +"</a>"
+                        + "</div>";
+                    }
+                    text = text + "</body>";
+                    consoleI(text, "#console");
+                }
+
             }
             else {
                 consoleT("status: " + data.statusText + " " + data.status + " " + data.getAllResponseHeaders(), "output", "#console");
@@ -80,6 +96,71 @@ $(document).ready(function(){
         else
             wikiList();
     });
+
+    $("#regenerateButton").click(function(event){
+        console.log("regenerate");
+         $.ajax({
+            url: "https://10.100.103.31:8243/token",
+            data: {"grant_type" : "client_credentials"},
+            headers: { "Authorization": "Basic X2dQMExoZmMwb2RITzFIUHJzN18yNE1Ha0w4YTpNREU5b0VKZzRabUs0SXJxNXZYQ2JZdFVKMUlh"},
+            //dataType: "json",
+            method: "POST",
+            error: function(jqXHR, textStatus, errorThrown){
+                consoleT(textStatus + ", " + jqXHR.status + ", ", "output", "#console");
+            }
+        }).complete(function(data) {
+            if (data.status == 200){
+                console.log("secko ok");
+            }
+            else {
+                console.log("nesecko ok");
+            }
+        });
+    });
+
+    $('.myTabs a').click(function (e) {
+        e.preventDefault();
+        $(this).tab('show');
+    })
+
+    $("#equalsButton").click(function() {
+        var operation = $('#calcSubForm label.active input').val();
+        var x = parseInt($("#x").val());
+        var y = parseInt($("#y").val());
+        if ((!isNaN(x)) && (!isNaN(y))){
+            var URL = "http://10.100.103.31:8280/calc/1.0/";
+            console.log("volaj http://10.100.103.31:8280/calc/1.0");
+            switch(operation){
+                case "1": URL = URL + "add";
+                        break;
+                case "2": URL = URL + "subtract";
+                        break;
+                case "3": URL = URL + "multiply";
+                        break;
+                case "4": URL = URL + "divide";
+            };
+            URL += "?x="+x+"&y="+y;
+            $.ajax({
+                url: URL,
+                headers: { "Authorization": "Bearer " + autorization},
+                //dataType: "xml",
+                method: "GET",
+                error: function(jqXHR, textStatus, errorThrown){
+                    //consoleT(textStatus + ", " + jqXHR.status + ", ", "output", "#console");
+                }
+            }).complete(function(data) {
+                if (data.status == 200){
+                    //consoleT(data.responseText,"ouput", "#console");
+                    $("#vysledok").val(data.responseJSON.answer);
+                }
+                else {
+                    console.log("status: " + data.statusText + " " + data.status + " " + data.getAllResponseHeaders());
+                }
+            });
+        }
+
+        return false;
+    })
 
     wikiList();
 })
